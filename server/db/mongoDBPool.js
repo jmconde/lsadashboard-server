7508ea2286fbed75381b8240e710b74edf1059fb
José Conde <jcondec@protonmail.com>
2021-11-29 23:38:23 +0100

    fix

--- server/db/mongoDBPool.js
+++ server/db/mongoDBPool.js
@@ -2,10 +2,10 @@ const { MongoClient } = require('mongodb');
 const uri = 'mongodb://root:qwerty123@192.168.1.124:37017/?maxPoolSize=20';
 
 let conn; 
-const client = new MongoClient(uri);
 
 module.exports = {
     getMongoConnection: async() => {
+        const client = new MongoClient(uri);
         return await client.connect();
     },
     getMongoDatabase: (client, db) => {
